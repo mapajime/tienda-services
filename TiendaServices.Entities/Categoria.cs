30716d7409e7fe6a6fc694109ff708085ea7f234
jimena <mapajime@gmail.com>
2022-05-10 18:26:22 -0500

    Adding project entities and classes Categoria, Ciudad

---
+++ TiendaServices.Entities/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TiendaServices.Entities
+{
+    public class Categoria
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
