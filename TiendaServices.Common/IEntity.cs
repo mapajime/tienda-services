46b1cc3e5608826396a44e6eea256c7352fdc78f
jimena <mapajime@gmail.com>
2022-05-11 13:45:45 -0500

    Adding project Coommmon and Interface IRepository

---
+++ TiendaServices.Common/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TiendaServices.Common
+{
+    public interface IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
