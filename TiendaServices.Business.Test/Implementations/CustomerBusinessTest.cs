5b50391ec8b3ad4010968befcd892804d5c1fe87
jimena <mapajime@gmail.com>
2022-05-16 13:18:02 -0500

    ÂModified unit test

--- TiendaServices.Business.Test/Implementations/CustomerBusinessTest.cs
+++ TiendaServices.Business.Test/Implementations/CustomerBusinessTest.cs
@@ -21,8 +21,8 @@ namespace TiendaServices.Business.Tests.Implementations
         [Fact]
         public async Task CreateCustomerAsync_WhenCustomerIsNull_ShouldThrowUserInvalidException()
         {
-            var customer = new CustomerBusiness(_mockCustomerRepository.Object);
-            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customer.CreateCustomerAsync(null));
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customerBusiness.CreateCustomerAsync(null));
 
             Assert.Contains("The customer is null", exception.Message);
             _mockCustomerRepository.Verify(c => c.CreateAsync(It.IsAny<Customer>()), Times.Never);
@@ -31,8 +31,8 @@ namespace TiendaServices.Business.Tests.Implementations
         [Fact]
         public async Task CreateCustomerAsync_WhenNameCustomerIsNull_ShouldThrowUserInvalidException()
         {
-            var customer = new CustomerBusiness(_mockCustomerRepository.Object);
-            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customer.CreateCustomerAsync(new Customer { Name = null, Email = "majsh@gmail.com", Phone = "32456789", CreationDate = DateTime.Now }));
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customerBusiness.CreateCustomerAsync(new Customer { Name = null, Email = "majsh@gmail.com", Phone = "32456789", CreationDate = DateTime.Now }));
             Assert.Contains("The name customer is null", exception.Message);
             _mockCustomerRepository.Verify(c => c.CreateAsync(It.IsAny<Customer>()), Times.Never);
         }
@@ -40,8 +40,8 @@ namespace TiendaServices.Business.Tests.Implementations
         [Fact]
         public async Task CreateCustomerAsync_WhenDocumentNumberCustomerIsNull_ShouldThrowUserInvalidException()
         {
-            var customer = new CustomerBusiness(_mockCustomerRepository.Object);
-            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customer.CreateCustomerAsync(new Customer { Name = "Gumercinda Lopez", DocumentNumber = null, Email = "jdsjas@gmail.com", Phone = "3456789098" }));
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customerBusiness.CreateCustomerAsync(new Customer { Name = "Gumercinda Lopez", DocumentNumber = null, Email = "jdsjas@gmail.com", Phone = "3456789098" }));
             Assert.Contains("The document number customer is null", exception.Message);
             _mockCustomerRepository.Verify(c => c.CreateAsync(It.IsAny<Customer>()), Times.Never);
         }
@@ -49,8 +49,8 @@ namespace TiendaServices.Business.Tests.Implementations
         [Fact]
         public async Task CreateCustomerAsync_WhenEmailCustomerIsNull_ShouldThrowUserInvalidException()
         {
-            var customer = new CustomerBusiness(_mockCustomerRepository.Object);
-            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customer.CreateCustomerAsync(new Customer { Name = "Ana Maria", DocumentNumber = "2300567", Email = null, Phone = "231456789" }));
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customerBusiness.CreateCustomerAsync(new Customer { Name = "Ana Maria", DocumentNumber = "2300567", Email = null, Phone = "231456789" }));
             Assert.Contains("The email customer is null", exception.Message);
             _mockCustomerRepository.Verify(c => c.CreateAsync(It.IsAny<Customer>()), Times.Never);
         }
@@ -58,8 +58,8 @@ namespace TiendaServices.Business.Tests.Implementations
         [Fact]
         public async Task CreateCustomerAsync_WhenPhoneCustomerIsNull_ShouldThrowUserInvalidException()
         {
-            var customer = new CustomerBusiness(_mockCustomerRepository.Object);
-            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customer.CreateCustomerAsync(new Customer { Name = "Maria Jose", DocumentNumber = "23211567", Email = "werty@gmail.com", Phone = null }));
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customerBusiness.CreateCustomerAsync(new Customer { Name = "Maria Jose", DocumentNumber = "23211567", Email = "werty@gmail.com", Phone = null }));
             Assert.Contains("The phone customer is null", exception.Message);
             _mockCustomerRepository.Verify(c => c.CreateAsync(It.IsAny<Customer>()), Times.Never);
         }
@@ -67,10 +67,37 @@ namespace TiendaServices.Business.Tests.Implementations
         [Fact]
         public async Task CreateCustomerAsync_WhenCustomerIsYounger_ShouldThrowUserInvalidException()
         {
-            var customer = new CustomerBusiness(_mockCustomerRepository.Object);
-            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customer.CreateCustomerAsync(new Customer { Name = "Ana Paula", DocumentNumber = "20944567", Email = "mjhdh@hotmail.com", Phone = "231456789", DateBirth = new DateTime(2015, 12, 1) }));
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var exception = await Assert.ThrowsAsync<UserInvalidException>(() => customerBusiness.CreateCustomerAsync(new Customer { Name = "Ana Paula", DocumentNumber = "20944567", Email = "mjhdh@hotmail.com", Phone = "231456789", DateBirth = new DateTime(2015, 12, 1) }));
             Assert.Contains("The customer is younger", exception.Message);
             _mockCustomerRepository.Verify(c => c.CreateAsync(It.IsAny<Customer>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task CreateCustomerAsync_WhenCustomerIsOk_ShouldReturnTrue()
+        {
+            Customer customer = null;
+            _mockCustomerRepository.Setup(m => m.CreateAsync(It.IsAny<Customer>()))
+                .Callback<Customer>(c => customer = c)
+                .ReturnsAsync(true);
+            var customerBusiness = new CustomerBusiness(_mockCustomerRepository.Object);
+            var response = await customerBusiness.CreateCustomerAsync(new Customer
+            {
+                Name = "Maria Patarroyo",
+                CreationDate = new DateTime(2018, 02, 3),
+                DateBirth = new DateTime(2000, 12, 12),
+                ModificationDate = new DateTime(2019, 1, 2),
+                DocumentNumber = "1234567",
+                Email = "maria2@gmail.com",
+                Phone = "2345678",
+                FKCountry = Guid.NewGuid(),
+                FKDocument = Guid.NewGuid(),
+                Active = false
+            }); ;
+            Assert.True(response);
+            Assert.NotNull(customer);
+            Assert.True(customer.Active);
+            Assert.Equal("Maria Patarroyo", customer.Name);
         }
     }
 }
