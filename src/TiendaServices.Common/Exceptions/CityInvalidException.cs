090064833846579e5979fabcc93475cb40d79b0b
jimena <mapajime@gmail.com>
2022-05-19 17:52:24 -0500

    Add new files

---
+++ src/TiendaServices.Common/Exceptions/CityInvalidException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TiendaServices.Common.Exceptions
+{
+    public class CityInvalidException : Exception
+    {
+        public CityInvalidException() : base("Country is invalid")
+        {
+        }
+
+        public CityInvalidException(string message) : base(message)
+        {
+        }
+    }
+}
--- src/TiendaServices.DataAccess/Interfaces/ICountryRepository.cs
+++ src/TiendaServices.DataAccess/Interfaces/ICountryRepository.cs
@@ -1,9 +1,11 @@
-﻿using TiendaServices.Common.Data;
+﻿using System.Threading.Tasks;
+using TiendaServices.Common.Data;
 using TiendaServices.Entities;
 
 namespace TiendaServices.DataAccess.Interfaces
 {
     public interface ICountryRepository : IRepository<Country>
     {
+        Task<Country> GetCountryByNameAsync(string name);
     }
 }
