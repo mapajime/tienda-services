0c6482e40d375e93326b5d5a83cb821bf4e3abd2
jimena <mapajime@gmail.com>
2022-05-25 10:37:20 -0500

    Adding Product business logic and unit test

--- src/TiendaServices.Business/Intarfaces/IProductBusiness.cs
+++ src/TiendaServices.Business/Intarfaces/IProductBusiness.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using TiendaServices.Common.Data;
 using TiendaServices.Entities;
 
-namespace TiendaServices.DataAccess.Interfaces
+namespace TiendaServices.Business.Intarfaces
 {
-    public interface IProductRepository : IRepository<Product>
+    public interface IProductBusiness
     {
+        Task<bool> CreateProductAsync(Product product);
+
+        Task<bool> UpdateProductAsync(Product product);
+
         Task<bool> DeactivateProduct(Guid id);
 
         Task<IEnumerable<Product>> GetAllActiveProductsByCategory(Guid idCategory);
 
         Task<IEnumerable<Product>> GetAllActiveProducts();
+
+        Task<IEnumerable<Product>> GetAllProductAsync();
+
+        Task<Product> GetProductByIdAsync(Guid id);
 
         Task<IEnumerable<Product>> GetProductsThatMatchNameAndAreActive(string name);
     }
---
+++ src/TiendaServices.Common/Exceptions/ProductInvalidException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TiendaServices.Common.Exceptions
+{
+    public class ProductInvalidException : Exception
+    {
+        public ProductInvalidException() : base("The producto is invalid")
+        {
+        }
+
+        public ProductInvalidException(string message) : base(message)
+        {
+        }
+    }
+}
